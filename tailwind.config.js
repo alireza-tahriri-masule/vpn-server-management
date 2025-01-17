b5c583644ced806f7cf276e33c50c521bd7dd1ae
Alireza Tahriri Masule <alirezatahriri4@gmail.com>
2025-01-18 01:15:46 +0330

    chore: Update Tailwind CSS configuration
    +‌ Add 'Kode Mono' font family
    + Modify responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ export default {
     ],
     theme: {
       extend: {},
+      container: {
+        center: true,
+        padding: '2rem'
+      },
+      fontFamily: {
+        english: ["Kode mono", "monospace"]
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      }
     },
     plugins: [],
   }
