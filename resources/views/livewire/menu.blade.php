be93691fc7dacae9b64e45db0f978e95254590c2
Alireza Tahriri Masule <alirezatahriri4@gmail.com>
2025-01-20 02:23:05 +0330

    feat: Remove unnecessary files from the project

--- resources/views/livewire/menu.blade.php
+++
@@ -1,7 +0,0 @@
-<ul class="flex items-center space-x-6 text-sm">
-    @foreach($menuItems as $item)
-        <li class="{{ $item['isActive'] === true ? 'bg-[#353740] px-4 py-2 rounded-lg' : '' }}">
-            <a href="#">{{ $item['title'] }}</a>
-        </li>
-    @endforeach
-</ul>
